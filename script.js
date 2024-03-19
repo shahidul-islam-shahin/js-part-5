bd092a1984baf7a1f06f4c5670373cd0e04d1d20
shahidul-islam-shahin <shahinshahidulislam7@gmail.com>
2024-03-19 21:24:58 +0600

    js part 5

---
+++ script.js
@@ -0,0 +1,5 @@
+let fullName = prompt('what is your last name?')
+let age = prompt('what is your age?')
+
+
+document.getElementById('html').innerHTML =  'I am ' + fullName + ', ' + 'I am '+ age + 'Years old';
